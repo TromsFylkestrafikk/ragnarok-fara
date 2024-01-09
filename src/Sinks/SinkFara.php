1aa7d03fd7e145d4e7a7e5f82ce90122208415ef
tft-kyrre <kyrre.andr.hansen@tffk.no>
2024-01-09 14:42:01 +0100

    Issue #7: WIP. API changes.

--- src/Sinks/SinkFara.php
+++ src/Sinks/SinkFara.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Ragnarok\Fara\Facades\FaraFiles;
+use Ragnarok\Sink\Models\SinkFile;
 use Ragnarok\Sink\Models\RawFile;
 use Ragnarok\Sink\Services\LocalFiles;
 use Ragnarok\Sink\Sinks\SinkBase;
@@ -48,19 +49,9 @@ class SinkFara extends SinkBase
     /**
      * @inheritdoc
      */
-    public function fetch(string $id): int
+    public function fetch(string $id): SinkFile|null
     {
-        $this->faraFiles->setPath($id);
-        $fileSize = 0;
-        foreach (FaraFiles::getData($id) as $table => $rows) {
-            $content = implode(PHP_EOL, $rows);
-            $file = $this->faraFiles->toFile($this->filename($table), $content);
-            if (!$file) {
-                return 0;
-            }
-            $fileSize += $file->size;
-        }
-        return $fileSize;
+        return null;
     }
 
     /**
@@ -101,37 +92,16 @@ class SinkFara extends SinkBase
     /**
      * @inheritdoc
      */
-    public function import(string $id): int
+    public function import(string $id, SinkFile $file): int
     {
-        $records = 0;
-        foreach ($this->getLocalFileList($id) as $filename) {
-            $content = $this->faraFiles->getDisk()->get($filename);
-            $records += FaraFiles::exportToDb($filename, $content);
-        }
-        return $records;
+        return 0;
     }
 
     /**
      * @inheritdoc
      */
-    public function deleteImport(string $id): bool
+    public function deleteImport(string $id, SinkFile $file): bool
     {
-        DB::table('fara_stat_load')->where('dat', $id)->delete();
-        DB::table('fara_stat_traffic_cust_profile')->where('dat', $id)->delete();
-        DB::table('fara_stat_traffic_income')->where('dat', $id)->delete();
-
-        // Truncate the 'static' tables when all other tables are empty.
-        $count = DB::table('fara_stat_load')->count();
-        $count += DB::table('fara_stat_traffic_cust_profile')->count();
-        $count += DB::table('fara_stat_traffic_income')->count();
-        if ($count === 0) {
-            DB::table('fara_basic_journey')->delete();
-            DB::table('fara_basic_line')->delete();
-            DB::table('fara_basic_stop')->delete();
-            DB::table('fara_basic_template')->delete();
-            DB::table('fara_company')->delete();
-            DB::table('fara_customer_profile')->delete();
-        }
         return true;
     }
 

e4b18ae91a98dc7274d03b7f17c068f0d9dce5cd
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2025-06-24 12:55:35 +0200

    Loop over fetched tables.

--- src/Services/FaraFiles.php
+++ src/Services/FaraFiles.php
@@ -2,19 +2,9 @@
 
 namespace Ragnarok\Fara\Services;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
-use Ragnarok\Fara\Models\BasicJourney;
-use Ragnarok\Fara\Models\BasicLine;
-use Ragnarok\Fara\Models\BasicStop;
-use Ragnarok\Fara\Models\BasicTemplate;
-use Ragnarok\Fara\Models\Company;
-use Ragnarok\Fara\Models\CustomerProfile;
-use Ragnarok\Fara\Models\EmvTransaction;
 use Ragnarok\Fara\Models\EmvTransRawTrans;
-use Ragnarok\Fara\Models\RawTransaction;
-use Ragnarok\Fara\Models\StatLoad;
-use Ragnarok\Fara\Models\StatTrafficCustProfile;
-use Ragnarok\Fara\Models\StatTrafficIncome;
 use Ragnarok\Sink\Traits\LogPrintf;
 
 class FaraFiles
@@ -24,67 +14,44 @@ class FaraFiles
 
     public function getData(string $id)
     {
+        // List of FARA connection models that are relatively straight forward
+        // to import, The value is a closure used to filter the eloquent
+        // builder. 
+        $simpleModels = [
+            'BasicJourney' => null,
+            'BasicLine' => null,
+            'BasicStop' => null,
+            'BasicTemplate' => null,
+            'Company' => null,
+            'CustomerProfile' => null,
+            'EmvTransaction' => fn (string $id, Builder $query) => $query->whereDate('transactiondatetime', $id),
+            'RawTransaction' => fn (string $id, Builder $query) => $query->whereDate('eventtimestamp', $id),
+            'StatLoad' => fn (string $id, Builder $query) => $query->where('dat', $id),
+            'StatTrafficCustProfile' => fn (string $id, Builder $query) => $query->where('dat', $id),
+            'StatTrafficIncome' => fn (string $id, Builder $query) => $query->where('dat', $id),
+        ];
+
         $data = [];
 
         $this->logPrintfInit("[Fara %s]: ", $id);
         $this->debug("Initializing import ...");
-        $columnNames = collect(BasicJourney::first())->keys()->toArray();
-        $data['basicjourney'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = BasicJourney::get()->toArray();
-        $this->convertToCsv($data, 'basicjourney', $rows);
-        $this->debug("Converting %s", 'basicjourney');
 
-        $columnNames = collect(BasicLine::first())->keys()->toArray();
-        $data['basicline'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = BasicLine::get()->toArray();
-        $this->convertToCsv($data, 'basicline', $rows);
-
-        $columnNames = collect(BasicStop::first())->keys()->toArray();
-        $data['basicstop'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = BasicStop::get()->toArray();
-        $this->convertToCsv($data, 'basicstop', $rows);
-
-        $columnNames = collect(BasicTemplate::first())->keys()->toArray();
-        $data['basictemplate'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = BasicTemplate::get()->toArray();
-        $this->convertToCsv($data, 'basictemplate', $rows);
-
-        $columnNames = collect(Company::first())->keys()->toArray();
-        $data['company'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = Company::get()->toArray();
-        $this->convertToCsv($data, 'company', $rows);
-
-        $columnNames = collect(CustomerProfile::first())->keys()->toArray();
-        $data['customerprofile'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = CustomerProfile::get()->toArray();
-        $this->convertToCsv($data, 'customerprofile', $rows);
-
-        $columnNames = collect(StatLoad::first())->keys()->toArray();
-        $data['statload'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = StatLoad::where('dat', $id)->get()->toArray();
-        $this->convertToCsv($data, 'statload', $rows);
-
-        $columnNames = collect(StatTrafficCustProfile::first())->keys()->toArray();
-        $data['stattrafficcustprofile'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = StatTrafficCustProfile::where('dat', $id)->get()->toArray();
-        $this->convertToCsv($data, 'stattrafficcustprofile', $rows);
-
-        $columnNames = collect(StatTrafficIncome::first())->keys()->toArray();
-        $data['stattrafficincome'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = StatTrafficIncome::where('dat', $id)->get()->toArray();
-        $this->convertToCsv($data, 'stattrafficincome', $rows);
-
-        $columnNames = collect(EmvTransaction::first())->keys()->toArray();
-        $data['emvtransaction'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = EmvTransaction::whereDate('transactiondatetime', $id)
-            ->get()
-            ->toArray();
-        $this->convertToCsv($data, 'emvtransaction', $rows);
-
-        $columnNames = collect(RawTransaction::first())->keys()->toArray();
-        $data['rawtransaction'] = [implode(self::SEPARATOR, $columnNames)];
-        $rows = RawTransaction::whereDate('eventtimestamp', $id)->get()->toArray();
-        $this->convertToCsv($data, 'rawtransaction', $rows);
+        foreach ($simpleModels as $modelBase => $filter) {
+            $this->debug("Fetching %s ...", $modelBase);
+            $mClass = 'Ragnarok\\Fara\\Models\\' . $modelBase;
+            $columnNames = collect(call_user_func($mClass . '::first'))
+                ->keys()
+                ->toArray();
+            $tableName = (new $mClass)->getTable();
+            $data[$tableName] = [implode(self::SEPARATOR, $columnNames)];
+            $rows = $filter
+                ? call_user_func(
+                    $mClass . '::where',
+                    fn (Builder $query) => $filter($id, $query)
+                )->get()->toArray()
+                : call_user_func($mClass . '::get')->toArray();
+            $this->convertToCsv($data, $tableName, $rows);
+        }
 
         $columnNames = collect(EmvTransRawTrans::first())->keys()->toArray();
         $data['emvtransrawtrans'] = [implode(self::SEPARATOR, $columnNames)];
